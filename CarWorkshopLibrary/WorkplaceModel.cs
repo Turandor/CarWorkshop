69bed648026bd87dd3dd43813a74808084358555
dorianjaniak1997 <dorianjaniak1997@gmail.com>
2021-01-06 20:58:35 +0100

    Added workplace

---
+++ CarWorkshopLibrary/WorkplaceModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarWorkshopLibrary
+{
+    public class WorkplaceModel
+    {
+        public int idWorkplace { get; set; }
+        public string workplaceName { get; set; }
+    }
+}
